bbc3c646851eccc203249930de87898baab38d08
Theo <theo@scribd.com>
2020-10-13 12:13:33 -0700

    Cleanup deduplicator

--- Lucid/Core/APIRequestDeduplicator.swift
+++ Lucid/Core/APIRequestDeduplicator.swift
@@ -27,34 +27,28 @@ public final class APIRequestDeduplicator: APIRequestDeduplicating {
     }
 
     public func testForDuplication(request: APIRequestConfig,
-                                   handler: @escaping (Result<APIClientResponse<Data>, APIError>) -> Void, completion: @escaping (Bool) -> Void) {
-
-        // keep response off internal data queue
-        var isADuplicate = false
-        defer { completion(isADuplicate) }
-
-        queue.sync {
+                                   handler: @escaping (Result<APIClientResponse<Data>, APIError>) -> Void,
+                                   completion: @escaping (Bool) -> Void) {
+        queue.async {
             if request.deduplicate && self._requestsInProgress.contains(request) {
                 var handlers = self._duplicateHandlers[request] ?? []
                 handlers.append(handler)
                 self._duplicateHandlers[request] = handlers
-                isADuplicate = true
+                completion(true)
             } else {
                 self._requestsInProgress.insert(request)
-                isADuplicate = false
+                completion(false)
             }
         }
     }
 
     public func applyResultToDuplicates(request: APIRequestConfig, result: Result<APIClientResponse<Data>, APIError>) {
-
-        // keep handler execution off internal data queue
-        var handlers = [(Result<APIClientResponse<Data>, APIError>) -> Void]()
-        defer { handlers.forEach { $0(result) } }
-
-        queue.sync {
+        queue.async {
             self._requestsInProgress.remove(request)
-            handlers = self._duplicateHandlers.removeValue(forKey: request) ?? []
+            let handlers = self._duplicateHandlers.removeValue(forKey: request) ?? []
+            for handler in handlers {
+                handler(result)
+            }
         }
     }
 }

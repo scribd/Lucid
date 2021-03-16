2fb8722a83d1f2e8cd9b447bf6be829c5b6bf5df
Theo <theo@scribd.com>
2021-03-16 16:13:45 -0700

    Updated bbn command to automatically update homebrew's version and sha

--- bin/bbn.sh
+++ bin/bbn.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-##
-# Use this script to increment the patch value in VERSION and create a commit for it.
-##
+current_version=`cat VERSION`
 
-set -e
-set -o pipefail
+next_version=$1
+if [ -z $next_version ]; then
+	next_version=${current_version%.*}.$((${current_version##*.}+1))
+fi
 
-# parse version
-MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
-MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
-PATCH=$(cat ./VERSION | xargs | cut -d'.' -f3)
+echo $next_version
+echo $next_version > VERSION
 
-# bump patch value
-PATCH=`expr $PATCH + 1`
-VERSION="${MAJOR}.${MINOR}.${PATCH}"
-printf "${VERSION}" > ./VERSION
+release_tar="https://github.com/scribd/Lucid/archive/$current_version.tar.gz"
+sha=$(curl -L -s $release_tar | shasum -a 256 | sed 's/ .*//')
+sed -i '' "s|^  \(sha256 \"\)\(.*\)\(\"\)|  \1$sha\3|" $(brew --repo homebrew/core)/Formula/lucid.rb
 
-# add commit
-git add VERSION
-git commit -m "Bump version to $VERSION"
+previous_version=${current_version%.*}.$((${current_version##*.}-1))
+sed -i '' "s/${previous_version//./\\.}/$current_version/g" \
+$(brew --repo homebrew/core)/Formula/lucid.rb
+
+git commit -am "Bump version to $next_version"
